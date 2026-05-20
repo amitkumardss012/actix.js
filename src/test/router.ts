1c92df39108068ba26e1432e79b418190694ef0c
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-05-20 17:10:07 +0530

    removed test

--- src/test/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "../router/router.js";
-
-const userRouter = new Router();
-
-userRouter.get("/home", (req, res) => {
-    return res.send("Hellow from user router");
-});
-
-export default userRouter;
